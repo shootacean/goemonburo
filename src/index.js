0c6a2c2a9766e5c1a6a7c9d5b1035a62cfc3a7ff
Shootacean <shootacean@gmail.com>
2018-12-16 18:07:08 +0900

    [add] Add simple todo page.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 'use strict';
 
-require('ace-css/css/ace.css');
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+// loads the Icon plugin
+UIkit.use(Icons);
+
+// components can be called from the imported UIkit reference
+UIkit.notification('Hello world.');
+
+require('uikit/dist/css/uikit.css');
 require('font-awesome/css/font-awesome.css');
 
 require('./index.html');
+require('./gtd.html');
 
 const {Elm} = require('./Main.elm');
 const mountNode = document.getElementById('main');
